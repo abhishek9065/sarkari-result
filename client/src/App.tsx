38dd775b14e274c62934b676290922085db43994
abhishek <www.abhisheksitamarhi@gmail.com>
2025-07-26 19:33:23 +0530

    Add Vercel Analytics integration to React app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline, Box } from '@mui/material';
+import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from './context/AuthContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -71,6 +72,7 @@ function App() {
           </Box>
         </Router>
       </AuthProvider>
+      <Analytics />
     </ThemeProvider>
   );
 }
